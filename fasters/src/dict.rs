0aad23049d6cb711da348ff28862afe7469eea2f
Filippo Costa <filippocosta.italy@gmail.com>
2020-02-11 05:57:47 +0100

    Add components to Dictionary

--- fasters/src/dict.rs
+++ fasters/src/dict.rs
@@ -15,13 +15,19 @@ struct Message {
 pub fn codegen(dict: Dictionary) -> String {
     let mut scope = Scope::new();
     for (_, field) in dict.fields {
-        let structure = scope.new_struct(field.name.as_str()).vis("pub");
+        let structure = scope
+            .get_or_new_module("fields")
+            .new_struct(field.name.as_str())
+            .vis("pub");
         if let Some(description) = field.description {
             structure.doc(description.as_str());
         }
     }
     for (name, message) in dict.messages {
-        let structure = scope.new_struct(name.as_str()).vis("pub");
+        let structure = scope
+            .get_or_new_module("messages")
+            .new_struct(name.as_str())
+            .vis("pub");
         structure.doc(message.description.as_str());
     }
     scope.to_string()
@@ -32,6 +38,7 @@ pub struct Dictionary {
     fields: HashMap<<types::Field as HasPk>::Pk, types::Field>,
     messages: HashMap<<types::Message as HasPk>::Pk, types::Message>,
     components: HashMap<<types::Component as HasPk>::Pk, types::Component>,
+    contents: HashMap<<types::Message as HasPk>::Pk, Vec<types::MsgContent>>,
 }
 
 impl Dictionary {
@@ -54,10 +61,18 @@ impl Dictionary {
                 .into_iter()
                 .map(|m| (m.pk(), m)),
         );
+        let mut contents = HashMap::new();
+        for mc in RepoV2010::msg_contents(version).data.into_iter() {
+            contents
+                .entry(mc.component_id.clone())
+                .or_insert_with(|| vec![])
+                .push(mc);
+        }
         Self {
             messages,
             fields,
             components,
+            contents,
         }
     }
 }
