a7ed8bd0e3da2c594fd9f148111488dd828e8a67
Filippo Costa <filippocosta.italy@gmail.com>
2020-12-29 02:47:04 +0100

    Draft some incoming message matchinig for sessions

--- fasters/src/session/classic.rs
+++ fasters/src/session/classic.rs
@@ -39,6 +39,8 @@ pub enum SessionProfile {
     LightweightFixt,
 }
 
+/// A standard-compliant FIX session processor (also known as FIX engine). It has
+/// support for all session profiles.
 pub struct Processor<R: io::Read, W: io::Write> {
     profile: SessionProfile,
     sender: String,
@@ -49,6 +51,85 @@ pub struct Processor<R: io::Read, W: io::Write> {
     last_receipt: Instant,
     input: R,
     output: W,
+}
+
+enum SequenceResetMode {
+    GapFill,
+    Reset,
+}
+
+pub struct ProcessorBuilder<R: io::Read, W: io::Write> {
+    profile: SessionProfile,
+    sender: String,
+    target: String,
+    hearbeat: Duration,
+    input: R,
+    output: W,
+    seq_reset_mode: SequenceResetMode,
+}
+
+impl<R: io::Read, W: io::Write> ProcessorBuilder<R, W> {
+
+    pub fn new(acceptor: String, initiator: String, input: R, output: W) -> Self {
+        ProcessorBuilder {
+            profile: SessionProfile::Fix4,
+            sender: initiator,
+            target: acceptor,
+            hearbeat: Duration::from_secs(30),
+            input,
+            output,
+            seq_reset_mode: SequenceResetMode::GapFill,
+        }
+    }
+
+    pub fn enable_fixt11(mut self) -> Self {
+        self.profile = SessionProfile::Fixt;
+        self
+    }
+
+    pub fn enable_fix4(mut self) -> Self {
+        self.profile = SessionProfile::Fix4;
+        self
+    }
+
+    pub fn enable_fix42(mut self) -> Self {
+        self.profile = SessionProfile::Fix42;
+        self
+    }
+
+    pub fn enable_lightweight_fixt(mut self) -> Self {
+        self.profile = SessionProfile::LightweightFixt;
+        self
+    }
+
+    pub fn with_heartbeat(mut self, heartbeat: Duration) -> Self {
+        self.hearbeat = heartbeat;
+        self
+    }
+
+    pub fn enable_gap_fill_mode(mut self) -> Self {
+        self.seq_reset_mode = SequenceResetMode::GapFill;
+        self
+    }
+
+    pub fn enable_reset_mode(mut self) -> Self {
+        self.seq_reset_mode = SequenceResetMode::Reset;
+        self
+    }
+
+    pub fn build(self) -> Processor<R, W> {
+        Processor {
+            profile: self.profile,
+            sender: self.sender,
+            target: self.target,
+            next_inbound: 1,
+            next_outbound: 1,
+            heartbeat: self.hearbeat,
+            last_receipt: Instant::now(),
+            input: self.input,
+            output: self.output,
+        }
+    }
 }
 
 impl<R: io::Read, W: io::Write> Processor<R, W> {
@@ -66,14 +147,52 @@ impl<R: io::Read, W: io::Write> Processor<R, W> {
         }
     }
 
-    fn process_incoming(&mut self, message: ir::Message) -> Result<()> {
-        let now = Instant::now();
+    fn heartbeat(&mut self, now: Instant) -> Result<()> {
         let interval = now.duration_since(self.last_receipt);
         self.last_receipt = now;
         if interval > self.heartbeat {
-            return Err(Error::Generic);
+            Err(Error::Generic)
+        } else {
+            Ok(())
         }
-        if let Some(ir::FixFieldValue::Int(1)) = message.fields.get(&35) {
+    }
+
+    fn process_incoming(&mut self, message: ir::Message) -> Result<()> {
+        self.heartbeat(Instant::now());
+        if let ir::FixFieldValue::String(msg_type) = message.fields.get(&35).unwrap() {
+            match msg_type.as_str() {
+                // `Heartbeat(35=0)`.
+                "0" => (),
+                // `Logon(35=A)`.
+                "A" => (),
+                // `TestRequest(35=1)`.
+                "1" => {
+                    let mut response = ir::Message::new();
+                    let id = message.fields.get(&112).unwrap();
+                    response.fields.insert(112, id.clone());
+                    self.send(response)?;
+                },
+                // `ResendRequest(35=2)`.
+                "2" => {
+                    let mut response = ir::Message::new();
+                    let seq_from = message.fields.get(&7).unwrap();
+                    let seq_to = message.fields.get(&16).unwrap();
+                    let range = seq_from ..= seq_to;
+                    self.send(response)?;
+                },
+                // `Reject(35=3)`.
+                "3" => {
+                    let mut response = ir::Message::new();
+                    response.fields.insert(45, ir::FixFieldValue::Int(self.next_inbound as i64));
+                    self.send(response)?;
+                },
+                // `SequenceReset(35=4)`.
+                "4" => (),
+                // `Logout(35=5)`.
+                "5" => (),
+                // Business-related message.
+                _ => (),
+            }
         }
         // If `MsgSeqNum(34)` is missing, a `Logout(35=5)` message should be
         // sent, terminating the FIX connection with the Text(58) field
