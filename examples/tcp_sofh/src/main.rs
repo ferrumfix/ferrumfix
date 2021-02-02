da0539d53e02e3a706aa8a5629ccd56684367c94
Filippo Costa <filippocosta.italy@gmail.com>
2021-02-02 23:48:53 +0100

    Fix styling in SOFH example

--- examples/tcp_sofh/src/main.rs
+++ examples/tcp_sofh/src/main.rs
@@ -1,25 +1,26 @@
 use fasters::codec::sofh::BufCodec;
+use fasters::codec::FramelessDecoder;
 
 use std::io;
 use std::net;
+
+fn main() -> io::Result<()> {
+    let listener = net::TcpListener::bind("0.0.0.0:0")?;
+    print_listening(listener.local_addr()?);
+    let reader = listener.accept()?.0;
+    let parser = BufCodec::new();
+    let mut frames = parser.frames_streamiter(reader);
+    while let Ok(frame) = frames.next() {
+        let frame = frame.as_ref().unwrap();
+        let payload = std::str::from_utf8(frame.payload()).unwrap();
+        println!("{}", payload);
+    }
+    Ok(())
+}
 
 fn print_listening(addr: net::SocketAddr) {
     println!(
         "Listening on port {} for SOFH-enclosed messages.",
         addr.port()
     );
-}
-
-fn main() -> io::Result<()> {
-    let listener = net::TcpListener::bind("0.0.0.0:0")?;
-    print_listening(listener.local_addr()?);
-    let _stream = listener.accept()?.0;
-    let _parser = BufCodec::new();
-    //let mut frames = parser.read_frames(stream);
-    //while let Some(frame_result) = frames.next() {
-    //    let frame = frame_result.as_ref().unwrap();
-    //    let payload = std::str::from_utf8(frame.payload()).unwrap();
-    //    println!("{}", payload);
-    //}
-    Ok(())
 }
