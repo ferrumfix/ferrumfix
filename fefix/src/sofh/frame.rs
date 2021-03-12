2c70e25a79425c9dbf7e7948c1512b4b0fe76a9c
Filippo Costa <filippocosta.italy@gmail.com>
2021-03-12 18:13:03 +0100

    test(SOFH): improve testing for SOFH encode&decode

--- fefix/src/sofh/frame.rs
+++ fefix/src/sofh/frame.rs
@@ -3,6 +3,7 @@ use std::convert::TryInto;
 use std::io;
 
 const HEADER_SIZE_IN_BYTES: usize = 6;
+const MAX_MESSAGE_SIZE_IN_BYTES: usize = u32::MAX as usize - HEADER_SIZE_IN_BYTES;
 
 /// An immutable view into a SOFH-enclosed message, complete with its
 /// encoding type tag and message.
@@ -19,6 +20,11 @@ pub struct Frame<'a> {
 impl<'a> Frame<'a> {
     /// Creates a new [`Frame`] with the given `encoding_type` and `message`.
     ///
+    /// # Panics
+    ///
+    /// This function panics if `message.len() > u32::MAX as usize - 6`. Permitting larger
+    /// messages would cause encoding issues.
+    ///
     /// # Examples
     ///
     /// ```
@@ -28,6 +34,7 @@ impl<'a> Frame<'a> {
     /// assert_eq!(frame.message().len(), 2);
     /// ```
     pub fn new(encoding_type: u16, message: &[u8]) -> Frame {
+        assert!(message.len() <= MAX_MESSAGE_SIZE_IN_BYTES);
         Frame {
             encoding_type,
             message,
@@ -68,14 +75,16 @@ impl<'a> Frame<'a> {
 
     /// Deserializes [`Self`] from `data`. Returns `None` if invalid. Zero-copy.
     ///
+    /// This function ignores trailing bytes that are not part of the message.
+    ///
     /// # Examples
     ///
     /// ```
     /// use fefix::sofh::Frame;
     ///
-    /// // Message_Length ->            -----------
-    /// // Encoding_Type ->                         ---------
-    /// // Message ->                                         --
+    /// // Message_Length:          -----------
+    /// // Encoding_Type:                       ---------
+    /// // Message:                                       --
     /// let frame = Frame::decode(&[0, 0, 0, 7, 0x0, 0x0, 42]).unwrap();
     /// assert_eq!(frame.message(), &[42]);
     /// ```
@@ -113,9 +122,10 @@ impl<'a> Frame<'a> {
     /// ```
     /// use fefix::sofh::Frame;
     ///
-    /// // Message_Length ->            -----------
-    /// // Encoding_Type ->                         ---------
-    /// // Message ->                                         --
+    /// // Message_Length:
+    /// //            -----------
+    /// // Encoding_Type:         ---------
+    /// // Message:                         --
     /// let bytes = &[0, 0, 0, 7, 0x0, 0x0, 42];
     /// let frame = Frame::decode(bytes).unwrap();
     /// let buffer = &mut Vec::new();
@@ -144,23 +154,82 @@ fn get_field_encoding_type(data: &[u8]) -> u16 {
 
 #[cfg(test)]
 mod test {
-    //use super::*;
+    use super::*;
+    use quickcheck::QuickCheck;
 
-    //#[test]
-    //fn decoder_returns_error_when_frame_has_len_lt_6() {
-    //    for len in 0..6 {
-    //        let header = encode_header(len, 0x4324);
-    //        let frame = Frame::decode(&header[..]);
-    //        assert!(frame.is_none());
-    //    }
-    //}
+    #[test]
+    fn information_retrieval_is_consistent_with_new() {
+        let frame = Frame::new(0x0, &[]);
+        assert_eq!(frame.encoding_type(), 0x0);
+        assert_eq!(frame.message(), [] as [u8; 0]);
+        let frame = Frame::new(0x1122, b"foobar");
+        assert_eq!(frame.encoding_type(), 0x1122);
+        assert_eq!(frame.message(), b"foobar");
+        let frame = Frame::new(u16::MAX, &[0]);
+        assert_eq!(frame.encoding_type(), u16::MAX);
+        assert_eq!(frame.message(), &[0]);
+    }
 
-    //#[test]
-    //fn decoder_accepts_frame_with_len_6() {
-    //    let header = encode_header(6, 0x4324);
-    //    for len in 0..6 {
-    //        let frame = Frame::decode(&header[..]);
-    //        assert!(frame.is_some());
-    //    }
-    //}
+    #[test]
+    #[should_panic]
+    fn new_with_size_u32_max() {
+        let data = vec![0; u32::MAX as usize];
+        Frame::new(0x0, &data[..]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn new_with_size_u32_max_minus_5() {
+        let data = vec![0; u32::MAX as usize - 5];
+        Frame::new(0x0, &data[..]);
+    }
+
+    #[test]
+    fn new_with_size_u32_max_minus_6() {
+        let data = vec![0; u32::MAX as usize - 6];
+        Frame::new(0x0, &data[..]);
+    }
+
+    #[test]
+    fn new_with_size_u32_max_minus_7() {
+        let data = vec![0; u32::MAX as usize - 7];
+        Frame::new(0x0, &data[..]);
+    }
+
+    #[test]
+    fn decode_incomplete_header() {
+        assert!(matches!(
+            Frame::decode(&[]),
+            Err(Error::Incomplete { needed: 6 })
+        ));
+        assert!(matches!(
+            Frame::decode(&[0, 0, 0]),
+            Err(Error::Incomplete { needed: 3 })
+        ));
+        assert!(matches!(
+            Frame::decode(&[0, 0, 0, 0, 0]),
+            Err(Error::Incomplete { needed: 1 })
+        ));
+    }
+
+    #[test]
+    fn decode_empty_message() {
+        let frame = Frame::decode(&[0, 0, 0, 6, 0, 0]).unwrap();
+        assert_eq!(frame.encoding_type(), 0);
+        assert_eq!(frame.message(), [] as [u8; 0]);
+    }
+
+    #[test]
+    fn encode_then_decode_should_have_no_effect() {
+        fn prop(encoding_type: u16, data: Vec<u8>) -> bool {
+            let frame = Frame::new(encoding_type, &data[..]);
+            let buffer = &mut vec![];
+            frame.encode(buffer).unwrap();
+            let frame_decoded = Frame::decode(&buffer[..]).unwrap();
+            frame_decoded.encoding_type() == encoding_type && frame_decoded.message() == &data[..]
+        }
+        QuickCheck::new()
+            .tests(1000)
+            .quickcheck(prop as fn(u16, Vec<u8>) -> bool)
+    }
 }
